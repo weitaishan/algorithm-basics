d19b120d493146e31595ab35847bbcf4fb66e64c
魏太山 <450785730@qq.com>
2019-09-04 18:25:01 +0800

    1.两数之和-查找表（字典）

--- Python/1.两数之和.py
+++ Python/1.两数之和.py
@@ -26,11 +26,29 @@ class Solution(object):
                     return [index1,index2]
 
 
+    """
+    使用查找表来解决该问题。
+
+设置一个 map 容器 record 用来记录元素的值与索引，然后遍历数组 nums。
+
+每次遍历时使用临时变量 complement 用来保存目标值与当前值的差值
+在此次遍历中查找 record ，查看是否有与 complement 一致的值，如果查找成功则返回查找值的索引值与当前变量的值 i
+如果未找到，则在 record 保存该元素与索引值 i
+
+    """
+    def twoSum2(self, nums, target):
+        record = {}
+        for index, value in enumerate(nums):
+            complement = target - value
+            if complement in record:
+                return [record[complement], index]
+            record[value] = index
+        return None
 
 solution = Solution()
-nums = [2,7,11,15]
+nums = [2,31,7,11,15]
 target = 9
-list = solution.twoSum(nums,target)
+list = solution.twoSum2(nums,target)
 print(list)
 
 """
